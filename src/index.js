610e3aaad95879cf0e1c36bb0153f840eaca334b
Devika R <devika.r@razorthink.com>
2019-06-24 15:16:01 +0530

    scss module changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   render() {
     return (
       <div className={styles.content}>
-        It works...Good!!!
+        It works...Good!!! Color Changed....
       </div>
     );
   }
